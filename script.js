fbae444f30fa6de417a841d5560c84a9c528100b
Cyebukayire <peacecyebukayire@gmail.com>
2021-09-24 19:49:16 +0200

    Add streaming video on browser

---
+++ script.js
@@ -0,0 +1,10 @@
+const video = document.getElementById('video')
+function startVideo() {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => video.srcObject = stream,
+      err => console.error(err)
+    )
+  }
+
+  startVideo()
